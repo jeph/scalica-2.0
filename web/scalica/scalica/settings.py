6d59197c26d0791d1c7a58e88df5d3b0d9caa6d6
Jeph <jephliu@jeph.io>
2019-12-09 23:22:36 -0500

    pls work

--- web/scalica/scalica/settings.py
+++ web/scalica/scalica/settings.py
@@ -152,4 +152,3 @@ AWS_STORAGE_BUCKET_NAME = 'scalica-photos'
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_CUSTOM_DOMAIN = 'scalica-photos.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
